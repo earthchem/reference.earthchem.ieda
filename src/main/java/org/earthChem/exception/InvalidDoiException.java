5c4a678e0f3f9f4e9f2906b4b1e22c1b4dc91adf
Bruce Sun <bsun_98@yahoo.com>
2014-03-07 10:31:57 -0500

    added new exception file

---
+++ src/main/java/org/earthChem/exception/InvalidDoiException.java
@@ -0,0 +1,15 @@
+package org.earthChem.exception;
+
+public class InvalidDoiException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6187518957425383334L;
+	
+	public InvalidDoiException(String msg)
+	{
+		super(msg);
+	}
+
+}
