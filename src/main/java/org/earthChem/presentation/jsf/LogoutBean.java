fbf732b1266645152280ac8fa6badde8e0465932
bhchen8 <bhchen@ldeo.columbia.edu>
2014-03-31 13:09:48 -0400

    Update logout
    1. created LogoutBean for logout;  2. Removed out UserAccountBean and
    its accociated files

---
+++ src/main/java/org/earthChem/presentation/jsf/LogoutBean.java
@@ -0,0 +1,24 @@
+package org.earthChem.presentation.jsf;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
+
+import org.earthChem.bll.ReferenceManager;
+import org.primefaces.context.RequestContext;
+
+/*********
+ * JSF Backing Bean for Logout Page
+ *
+ *
+ */
+public class LogoutBean {
+
+	public String logout(){
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		return "/login.xhtml/logout?faces-redirect=true";
+	}
+}
