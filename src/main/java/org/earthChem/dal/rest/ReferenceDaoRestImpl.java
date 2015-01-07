444603a4fde5bea6701cb2dd78d7f795ca982c72
lulin-song <lulin.song@gmail.com>
2015-01-07 14:19:06 -0500

    removed data_entered column from reference
    Add and Edit page, data_entered input entries are removed since this
    column is removed from reference in database.

--- src/main/java/org/earthChem/dal/rest/ReferenceDaoRestImpl.java
+++ src/main/java/org/earthChem/dal/rest/ReferenceDaoRestImpl.java
@@ -11,7 +11,7 @@ import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
+
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.protocol.HttpContext;
